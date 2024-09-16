291b791952cf2fb500b9d63dfdc05e2abb665cef
Slapszrz <simon.martin.akmk@gmail.com>
2024-09-16 21:33:12 +0200

    Added /gif

--- src/classes/Display.js
+++ src/classes/Display.js
@@ -1,6 +1,13 @@
-const LedMatrix = require("easybotics-rpi-rgb-led-matrix");
+// const LedMatrix = require("easybotics-rpi-rgb-led-matrix");
+class LedMatrix {
+    constructor(height, width) {
+    }
+}
+
 const pixels = require('image-pixels');
 const decodeGif = require("decode-gif");
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 class Display {
     constructor(height, width) {
@@ -31,6 +38,21 @@ class Display {
 
         const rgbArray = new Uint8Array(data.filter((item, index) => ((index+1)%4 != 0)))
 
+        this.displayUint8Array(rgbArray, width, height)
+    }
+
+    async gif(base64Data) {
+        const { width, height, frames } = decodeGif(Buffer.from(base64Data, "base64"));
+
+        for (const frame in frames) {
+            this.displayUint8Array(frame, width, height)
+            await sleep(frame.timeCode)
+        }
+
+        console.log(frames[0].data[0])
+    }
+
+    displayUint8Array(rgbArray, width, height) {
         for (let i=0; i < width * height; i++) {
             const j = i * 3
 
@@ -43,6 +65,7 @@ class Display {
         }
         this.update()
     }
+
 }
 
 module.exports.Display=Display
